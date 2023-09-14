fdc4bd89cd73c3cf9990d69cd277304e6ab893f8
thuthu-c <thuannyba@gmail.br>
2023-09-13 23:14:35 -0300

    feature: add analisecontroller

---
+++ app/app/Http/Controllers/AnaliseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AnaliseController extends Controller
+{
+    public function mostrarAnalise(){
+        echo "<h1> Oi, eu sou a Análise!! </h1>";
+    }
+}
